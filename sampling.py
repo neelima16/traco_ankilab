980fb800d0036b219c1b438f4235e91755319ad3
haaguileraa <haaguileraa@unal.edu.co>
2022-07-25 22:06:34 +0200

    test videos 005-020

--- sampling.py
+++ sampling.py
@@ -2,8 +2,8 @@ import numpy as np
 import ffmpeg
 import os
 
-directory = './videos/'
-i = 51
+directory = './Final'
+i = 1
 for f in os.listdir(directory):
     f = os.path.join(directory, f)
     try:
@@ -11,7 +11,7 @@ for f in os.listdir(directory):
         #ffmpeg.input(f).trim(start=0, duration=10).filter('fps', fps=10, round='up').output('./training/training0{0}.mp4'.format(i)).run()
 
         #Test videos' template:
-        ffmpeg.input(f).trim(start=0, duration=10).filter('fps', fps=10, round='up').output('./test/test0{0}.mp4'.format(i)).run()
+        ffmpeg.input(f).trim(start=0, duration=10).filter('fps', fps=10, round='up').output('./Final/final00{0}.mp4'.format(i)).run()
     except Exception as e:
         print(e)
     i +=1
