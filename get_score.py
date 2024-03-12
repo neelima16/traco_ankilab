a18ef99575aed6c63c7f8bf5a845105a236dece5
Steckdose007 <florian@gritsch.eu>
2024-03-12 11:41:24 +0100

    Streak and target points implemented. Testing to be continued......

--- get_score.py
+++ get_score.py
@@ -2,15 +2,17 @@ import numpy as np
 import pandas as pd
 import logging
 
-from scipy.spatial.distance import cdist, euclidean
+from scipy.spatial.distance import cdist
 from scipy.optimize import linear_sum_assignment
 
-PENALTY_N_WRONG_HEXBUGS = 1_000  # only applies if the amount of predicted hexbugs is between 1 and 4
-PENALTY_MORE_THAN_FOUR_HEXBUGS = 1_000_000
-PENALTY_WRONG_NUMBER_FRAMES = 10_000
-
-
-def get_score(path_to_prediction: str, path_to_gt: str, log: bool = False) -> int:
+PENALTY_N_WRONG_HEXBUGS = -10_000  # only applies if the amount of predicted hexbugs is between 1 and 4
+PENALTY_MORE_THAN_FOUR_HEXBUGS = -10_000
+PENALTY_WRONG_NUMBER_FRAMES = -10_000
+#Rings in Pixel
+RINGS=          [1,  5 ,10,15,20,25]
+POINTS_PER_RING=[100,50,25,15,10,5]
+STREAK_POINTS= [0,2,40,60,200]
+def get_score_fct(path_to_prediction: str, path_to_gt: str, log: bool = False) -> int:
     """
     Calculate the score for the given prediction and ground truth files.
     :param path_to_prediction: Path to the prediction .csv file.
@@ -45,7 +47,8 @@ def get_score(path_to_prediction: str, path_to_gt: str, log: bool = False) -> in
     # Get the number of frames and check if they are equal
     n_frames_gt = gt_df['t'].max() + 1
     n_frames_pred = pred_df['t'].max() + 1
-
+    """
+    UNSICHER WIE SINNVOLL MIT EINBAUEN  
     if n_frames_gt != n_frames_pred:
         final_score += PENALTY_WRONG_NUMBER_FRAMES * np.abs(n_frames_gt - n_frames_pred)
 
@@ -56,9 +59,11 @@ def get_score(path_to_prediction: str, path_to_gt: str, log: bool = False) -> in
         # set n_frames depending on which one is smaller
         n_frames = min(n_frames_gt, n_frames_pred)
     else:
-        n_frames = n_frames_gt
-
+        n_frames = n_frames_gt"""
+    n_frames = n_frames_gt
     # Loop over all frames
+    ids_for_streak = {i: [50, False, 0] for i in range(10)}# Maximum of 10 hexbugs?
+    firstframe = True
     for idx in range(n_frames):
         if log:
             logger.info(f"Frame {idx}")
@@ -70,6 +75,11 @@ def get_score(path_to_prediction: str, path_to_gt: str, log: bool = False) -> in
         # Check the number of hexbugs and apply penalties if necessary
         n_hexbugs_gt = frame_gt_df['hexbug'].nunique()
         n_hexbugs_pred = frame_pred_df['hexbug'].nunique()
+        print(frame_pred_df['hexbug'])
+        #used to connect ids with the entries of the distance matrix
+        ids_gt = frame_gt_df['hexbug']
+        ids_pred = frame_pred_df['hexbug']
+
         if n_hexbugs_pred > 4:
             # To prevent submissions with a lot of random guesses
             final_score += PENALTY_MORE_THAN_FOUR_HEXBUGS
@@ -86,13 +96,42 @@ def get_score(path_to_prediction: str, path_to_gt: str, log: bool = False) -> in
                                 list(frame_pred_df[['x', 'y']].values), 'euclidean')
 
         # Calculate the final score
-        row_ind, col_ind = linear_sum_assignment(distance_matrix)  # Hungarian algorithm
-        for i, j in zip(row_ind, col_ind):
-            final_score += distance_matrix[i, j]
-            if log:
-                logger.info(f"Distance between hexbug {frame_gt_df['hexbug'].iloc[i]} and "
-                            f"{frame_pred_df['hexbug'].iloc[j]}: {distance_matrix[i, j]}")
+        gt_hex, pred_hex = linear_sum_assignment(distance_matrix)  # Hungarian algorithm
+        #print(ids_for_streak)
+        for i, j in zip(gt_hex, pred_hex):
+            gt_id = ids_gt.iloc[i]
+            pred_id = ids_pred.iloc[j]
+            if(ids_for_streak[gt_id][0] == pred_id):
+                ids_for_streak[gt_id][1] = True # the streak is on
+                ids_for_streak[gt_id][2] += 1
+            else:
+                ids_for_streak[gt_id][0] = pred_id
+                ids_for_streak[gt_id][1] = False  # the streak is on
+                ids_for_streak[gt_id][2] = 0
 
+            distance = distance_matrix[i, j]
+            points_recived = 1
+            #Score per Ring in which the prediction is
+            for intervalls in range(len(RINGS) - 1):
+                if RINGS[intervalls] < distance <= RINGS[intervalls + 1]:
+                    points_recived = POINTS_PER_RING[intervalls+1]
+                    if(distance<1):
+                        points_recived = 100
+                    break
+
+            #Look which streak
+            for intervall in range(len(STREAK_POINTS) - 1):
+                if STREAK_POINTS[intervall] < ids_for_streak[i][2] <= STREAK_POINTS[intervall + 1]:
+                    points_recived = points_recived *(intervall+ 1)
+                    break
+
+            final_score += points_recived
+
+            if log:
+                logger.info(f"Distance between hexbug {gt_id} and "
+                            f"{pred_id}: {distance_matrix[i, j]}   "
+                            f"Points recived : {points_recived}   "
+                            f" Hexbug on Fire: {ids_for_streak[gt_id][1]}")
         if log:
             logger.info("")
 
@@ -125,64 +164,20 @@ def _validate_dataframe_structure(df: pd.DataFrame, required_columns: list) -> b
         return False
 
 
-def get_score_with_id_matching(path_to_prediction: str, path_to_gt: str) -> int:
-    """
-    OLD CODE SNIPPET WHICH IS NOT RELEVANT ANYMORE.
-    """
-    raise NotImplementedError("This function is not relevant anymore.")
-
-    final_score = 0
-
-    # Load the data
-    pred_df = pd.read_csv(path_to_prediction, index_col=0)
-    gt_df = pd.read_csv(path_to_gt, index_col=0)
-
-    # Determine how many frames we have and how many hexbugs are used in the video of the ground truth
-    n_frames = gt_df['t'].max()
-    n_hexbugs_gt = gt_df['hexbug'].nunique()
-    n_hexbugs_pred = pred_df['hexbug'].nunique()
-
-    # Apply penalties if the amount of predicted hexbugs is wrong
-    if n_hexbugs_pred < 1:
-        final_score += PENALTY_ZERO_HEXBUGS
-    elif n_hexbugs_pred > 4:
-        final_score += PENALTY_MORE_THAN_FOUR_HEXBUGS
-    else:
-        final_score += (np.abs(n_hexbugs_pred - n_hexbugs_gt) * PENALTY_N_WRONG_HEXBUGS)
-
-        # Match the predicted hexbugs in the first frame with the ground truth
-    # --> the matching IDs are used in all the following frames
-    distance_matrix = cdist(list(gt_df[gt_df['t'] == 0][['x', 'y']].values),
-                            list(pred_df[pred_df['t'] == 0][['x', 'y']].values))
-    row_ind, col_ind = linear_sum_assignment(distance_matrix)  # Hungarian algorithm
-
-    matched_ids = {}
-    for i, j in zip(row_ind, col_ind):
-        matched_ids[f"{i}"] = j
-
-    # Iterate over each frame and calculate distances
-    for i in range(n_frames):
-        frame_gt_df = gt_df[gt_df['t'] == i]
-        frame_pred_df = pred_df[pred_df['t'] == i]
-        for j in range(n_hexbugs_gt):
-            row_gt = frame_gt_df[frame_gt_df['hexbug'] == j]
-            row_pred = frame_pred_df[frame_pred_df['hexbug'] == matched_ids[str(j)]]
-            score = euclidean(list(row_gt[['x', 'y']].values[0]), list(row_pred[['x', 'y']].values[0]))
-            final_score += score
-
-    return final_score
-
-
 # define main function call
 if __name__ == "__main__":
     # extract args
-    import argparse
+    # import argparse
+    #
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("path_to_prediction", help="path to the prediction file")
+    # parser.add_argument("path_to_gt", help="path to the ground truth file")
+    # parser.add_argument("--log", help="log the score", action="store_true")
+    # args = parser.parse_args()
+    # path_pred = "predicted_data_for_testing_scorecalc.csv"
+    # path_test = "test_data_csv/test001.csv"
+    path_pred = "test_score/test_onegoes_samecomes.csv"
+    path_test = "test_score/ground_trouth.csv"
+    #print(f"Score: {get_score(args.path_to_prediction, args.path_to_gt, args.log)}")
+    print(f"Score: {get_score_fct(path_pred, path_test, log = True)}")
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("path_to_prediction", help="path to the prediction file")
-    parser.add_argument("path_to_gt", help="path to the ground truth file")
-    parser.add_argument("--log", help="log the score", action="store_true")
-    args = parser.parse_args()
-
-    # calculate score
-    print(f"Score: {get_score(args.path_to_prediction, args.path_to_gt, args.log)}")
